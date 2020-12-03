77a4a6c7d7acc7ba06021b3aabb72132bdebc5b9
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-12-02 18:39:32 -0600

    Add Endpoint for Posts and Comments & json files for testing

--- models/post.js
+++ models/post.js
@@ -1,8 +1,42 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
+const commentSchema = new Schema({
+    author: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+    }
+}, {
+    timestamps: true
+});
 
+const postSchema = new Schema({
+    author: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    caption: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true
 });
 
 const Post = mongoose.model('Post', postSchema);
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,46 +1,217 @@
 const express = require('express');
+const Post = require ('../models/post');
+
 const postRouter = express.Router();
 
 postRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req, res) => {
-    res.end('Will send all the posts to you');
+    Post.find()
+    .then(posts => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(posts);
+    })
+    .catch(err => next(err));
 })
 .post((req, res) => {
-    res.end(`Will add the post: ${req.body.name} with description: ${req.body.description}`);
+    Post.create(req.body)
+    .then(post => {
+        console.log('Post Created ', post);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(post);
+    })
+    .catch(err => next(err));
 })
 .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /posts');
 })
 .delete((req, res) => {
-    res.end('Deleting all posts');
+    Post.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
 
 postRouter.route('/:postId')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req, res) => {
-    res.end(`Will send details of the post: ${req.params.postId} to you`);
+    Post.findById(req.params.postId)
+    .then(post => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(post);
+    })
+    .catch(err => next(err));
 })
 .post((req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /posts/${req.params.postId}`);
 })
 .put((req, res) => {
-    res.write(`Updating the post: ${req.params.postId}\n`);
-    res.end(`Will update the post: ${req.body.name} 
-        with description: ${req.body.description}`);
+    Post.findByIdAndUpdate(req.params.postId, {
+        $set: req.body
+    }, { new: true })
+    .then(post => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(post);
+    })
+    .catch(err => next(err));
 })
 .delete((req, res) => {
-    res.end(`Deleting post: ${req.params.postId}`);
+    Post.findByIdAndDelete(req.params.postId)
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+});
+
+postRouter.route('/:postId/comments')
+.get((req, res, next) => {
+    Post.findById(req.params.postId)
+    .then(post => {
+        if (post) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(post.comments);
+        } else {
+            err = new Error(`Post ${req.params.postId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.post((req, res, next) => {
+    Post.findById(req.params.postId)
+    .then(post => {
+        if (post) {
+            campsite.comments.push(req.body);
+            post.save()
+            .then(post => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(post);
+            })
+            .catch(err => next(err));
+        } else {
+            err = new Error(`Post ${req.params.postId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /posts/${req.params.postId}/comments`);
+})
+.delete((req, res, next) => {
+    Post.findById(req.params.postId)
+    .then(post => {
+        if (post) {
+            for (let i = (post.comments.length-1); i >= 0; i--) {
+                post.comments.id(post.comments[i]._id).remove();
+            }
+            post.save()
+            .then(post => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(post);
+            })
+            .catch(err => next(err));
+        } else {
+            err = new Error(`Campsite ${req.params.campsiteId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+});
+
+
+postRouter.route('/:postId/comments/:commentId')
+.get((req, res, next) => {
+    Post.findById(req.params.postId)
+    .then(post => {
+        if (post && post.comments.id(req.params.commentId)) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(post.comments.id(req.params.commentId));
+        } else if (!post) {
+            err = new Error(`Post ${req.params.postId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /posts/${req.params.postId}/comments/${req.params.commentId}`);
+})
+.put((req, res, next) => {
+    Post.findById(req.params.postId)
+    .then(post => {
+        if (post && post.comments.id(req.params.commentId)) {
+            if (req.body.rating) {
+                post.comments.id(req.params.commentId).rating = req.body.rating;
+            }
+            if (req.body.text) {
+                post.comments.id(req.params.commentId).text = req.body.text;
+            }
+            post.save()
+            .then(post => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(post);
+            })
+            .catch(err => next(err));
+        } else if (!campsite) {
+            err = new Error(`Post ${req.params.postId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.delete((req, res, next) => {
+    Post.findById(req.params.postId)
+    .then(post => {
+        if (post && post.comments.id(req.params.commentId)) {
+            post.comments.id(req.params.commentId).remove();
+            post.save()
+            .then(post => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(post);
+            })
+            .catch(err => next(err));
+        } else if (!post) {
+            err = new Error(`post ${req.params.postId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
 });
 
 module.exports = postRouter;
